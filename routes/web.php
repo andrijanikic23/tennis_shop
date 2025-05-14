596fbbef2d15ff764ebee4f523cc3016e558ad28
andrijanikic23 <andrijanikic001@gmail.com>
2025-05-14 18:32:03 +0200

    * Routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +15,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::view('/products/new', 'addProduct');
+
+Route::post('/products/added', [\App\Http\Controllers\ProductsController::class, 'newProduct'])
+    ->name('products.added');
+
+
+Route::view('/products/search', 'welcome');
+Route::get('/products/search-results', [\App\Http\Controllers\ProductsController::class, 'searchResults'])
+    ->name('products.search');
+Route::get('products/singleProduct/{productId}', [\App\Http\Controllers\ProductsController::class, 'singleProduct'])
+    ->name('products.singleProduct');
+Route::post('/cart/overview', [\App\Http\Controllers\ShoppingCartController::class, 'addProductToCart'])
+    ->name('cart.add');
+
 require __DIR__.'/auth.php';
+
+
+
